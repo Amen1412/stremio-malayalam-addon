022dd102993a7828c45696cfc17efd57581eae59
Amen1412 <anonyamen@gmail.com>
2025-05-23 00:01:57 +0530

    app.py

--- app.py
+++ app.py
@@ -144,5 +144,8 @@ def refresh():
 fetch_movies("ml")
 fetch_movies("hi")
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
